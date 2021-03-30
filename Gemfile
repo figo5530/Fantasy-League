fa93cb84c2f236d48a0f8e3180a73507776506f8
figo5530 <figooooo0221@gmail.com>
2021-03-29 21:03:42 -0400

    adds omniauth, omniauth-facebook, dotenv-rails in gemfile, sets up .env and omniauth.rb, adds callback route in routes.

--- Gemfile
+++ Gemfile
@@ -22,7 +22,9 @@ gem 'uglifier', '>= 1.3.0'
 
 gem 'pry'
 # bootstrap
-
+gem 'omniauth'
+gem 'omniauth-facebook'
+gem 'dotenv-rails'
 gem 'bootstrap-sass', '3.3.7'
 
 # Use CoffeeScript for .coffee assets and views
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  get '/auth/facebook/callback', to: 'sessions#create_with_facebook'
 end
